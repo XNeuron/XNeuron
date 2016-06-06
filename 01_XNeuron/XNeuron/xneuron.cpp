0705d7a1aa25862ede91c1fe671fca283d712685
Xma1 <xma1@xma1.de>
2016-06-07 00:37:04 +0200

    [impl] new Neuron

--- 01_XNeuron/XNeuron/xneuron.cpp
+++ 01_XNeuron/XNeuron/xneuron.cpp
@@ -4,128 +4,158 @@ XNeuron::XNeuron()
 {
 }
 
-bool XNeuron::train(QList<QList<bool>> &input, QList<bool> &mOutputRequired)
+bool XNeuron::train(QList<QList<bool>> &xInput, QList<bool> &xOutputRequired)
 {
-    bool ok=false;
-    for (int var = 0; true; ++var)
-    {
-        for (QList<bool>& x: input)
-        {
-            setInput(x);
-            double Required =mOutputRequired[input.indexOf(x)];
-            if(outputBinary()!=Required)
-            {
-                ok=false;
-                for (double& w: mWeight)
-                {
-                    double dx=x[mWeight.indexOf(w)]?1.0:0.0;
-                    w+=(Required-outputReal())*dx/mWeight.length();
-                }
-                break;
-            }else
-            {
-                ok=true;
-            }
-        }
-        if(ok)
-            return true;
-    }
+	QList<QList<double>> mInput;
+	QList<double> mOutputRequired;
 
-    return false;
+	for (QList<bool>& x : xInput)
+	{
+		QList<double> temp;
+		for (bool& y : x)
+		{
+			temp.append((y) ? (1) : (0));
+		}
+		mInput.append(temp);
+		mOutputRequired.append((xOutputRequired[xInput.indexOf(x)]) ? (1) : (0));
+	}
+	return train(mInput, mOutputRequired);
+}
+
+bool XNeuron::train(QList<QList<double>> &input, QList<double> &mOutputRequired)
+{
+	for (int var = 0; 1000; ++var)
+	{
+		double error = 0;
+		for (QList<double>& x : input)
+		{
+			double temp = 0;
+			for (int i = 0; i < x.length(); i++)//double& value : x)
+			{
+				temp += x[i]*mWeight[i];
+			}
+
+			double y = (temp >= mBase) ? (1) : (0);
+
+			if (y != mOutputRequired[input.indexOf(x)]) 
+			{
+
+				double delta = mOutputRequired[input.indexOf(x)] - y;
+
+				mBase = mBase - delta;
+
+				for (int i = 0; i < mWeight.length();i++)//double& value : mWeight)
+				{
+					mWeight[i] = mWeight[i] + delta*x[i];
+				}
+
+				error ++;
+			}
+		}
+		if (error <= 0)
+			return true;
+	}
+
+	return false;
 }
 
 double XNeuron::outputReal() const
 {
-    return mOutputReal;
+	return mOutputReal;
 }
 
 bool XNeuron::outputBinary() const
 {
-    return (mOutputReal<0.75?0:1);
+	return   mOutputReal >= mBase;//(mOutputReal < 0.75 ? 0 : 1);
 }
 
 void XNeuron::setOutputReal(double outputReal)
 {
-    mOutputReal = outputReal;
+	mOutputReal = outputReal;
 }
 
 QList<double> XNeuron::input() const
 {
-    return mInput;
+	return mInput;
 }
 
 void XNeuron::setInput(bool A, bool B)
 {
-    setInput(QList<bool>()<<A<<B);
+	setInput(QList<bool>() << A << B);
 }
 
 void XNeuron::setInput(const QList<bool> &input)
 {
-    QList<double> tInput;
-    clearWeight(input);
-    for (bool elem: input)
-    {
-        initWeight(input);
-        tInput << (elem?1.0:0.0);
-    }
-    setInput(tInput);
+	QList<double> tInput;
+	clearWeight(input);
+	for (bool elem : input)
+	{
+		initWeight(input);
+		tInput << (elem ? 1.0 : 0.0);
+	}
+	setInput(tInput);
 }
 
 void XNeuron::initWeight(const QList<double> &input)
 {
-    if(mWeight.length()!=input.length())
-    {
-        mWeight.append(0.5);
-    }
+	if (mWeight.length() != input.length())
+	{
+		mWeight.append(0.5);
+	}
 }
 
 void XNeuron::clearWeight(const QList<double> &input)
 {
-    if(mWeight.length()!=input.length())
-    {
-        mWeight.clear();
-    }
+	if (mWeight.length() != input.length())
+	{
+		mWeight.clear();
+	}
 }
 
 void XNeuron::initWeight(const QList<bool> &input)
 {
-    if(mWeight.length()!=input.length())
-    {
-        mWeight.append(0.5);
-    }
+	if (mWeight.length() != input.length())
+	{
+		mWeight.append(0.5);
+	}
 }
 
 void XNeuron::clearWeight(const QList<bool> &input)
 {
-    if(mWeight.length()!=input.length())
-    {
-        mWeight.clear();
-    }
+	if (mWeight.length() != input.length())
+	{
+		mWeight.clear();
+	}
 }
 
 QList<double> XNeuron::weight() const
 {
-    return mWeight;
+	return mWeight;
 }
 
 void XNeuron::setWeight(double A, double B)
 {
-    mWeight = QList<double>()<<A<<B;
+	mWeight = QList<double>() << A << B;
+}
+
+void XNeuron::setWeight(double A, double B, double C)
+{
+	mWeight = QList<double>() << A << B << C;
 }
 
 void XNeuron::setWeight(const QList<double> &weight)
 {
-    mWeight = weight;
+	mWeight = weight;
 }
 
 void XNeuron::setInput(const QList<double> &input)
 {
-    mInput = input;
-    clearWeight(input);
-    mOutputReal=0;
-    for (const double& elem: input)
-    {
-        initWeight(input);
-        mOutputReal+= elem*mWeight[input.indexOf(elem)];
-    }
+	mInput = input;
+	clearWeight(input);
+	mOutputReal = 0;
+	for (const double& elem : input)
+	{
+		initWeight(input);
+		mOutputReal += elem*mWeight[input.indexOf(elem)];
+	}
 }
--- 01_XNeuron/XNeuron/xneuron.h
+++ 01_XNeuron/XNeuron/xneuron.h
@@ -8,36 +8,39 @@ class /*XNEURONSHARED_EXPORT*/ XNeuron
 {
 
 public:
-    XNeuron();
+	XNeuron();
 
-    QList<XNeuron> mNextNeuron;
+	QList<XNeuron> mNextNeuron;
 
-    bool train(QList<QList<bool>> &input, QList<bool> &mOutputRequired);
+	bool train(QList<QList<bool>> &input, QList<bool> &mOutputRequired);
+	bool train(QList<QList<double>> &input, QList<double> &mOutputRequired);
 
-    double outputReal() const;
-    bool outputBinary() const;
-    void setOutputReal(double outputReal);
+	double outputReal() const;
+	bool outputBinary() const;
+	void setOutputReal(double outputReal);
 
-    QList<double> input() const;
-    void setInput(bool A,bool B);
-    void setInput(const QList<bool> &input);
-    void setInput(const QList<double> &input);
+	QList<double> input() const;
+	void setInput(bool A, bool B);
+	void setInput(const QList<bool> &input);
+	void setInput(const QList<double> &input);
 
-    void initWeight(const QList<bool> &input);
-    void clearWeight(const QList<bool> &input);
+	void initWeight(const QList<bool> &input);
+	void clearWeight(const QList<bool> &input);
 
-    void initWeight(const QList<double> &input);
-    void clearWeight(const QList<double> &input);
+	void initWeight(const QList<double> &input);
+	void clearWeight(const QList<double> &input);
 
-    QList<double> weight() const;
-    void setWeight(double A,double B);
-    void setWeight(const QList<double> &weight);
+	QList<double> weight() const;
+	void setWeight(double A, double B);
+	void setWeight(double A, double B, double C);
+	void setWeight(const QList<double> &weight);
 
 private:
-    QList<double> mInput;
-    double mOutputReal;
-    double mOutputRequired;
-    QList<double> mWeight;
+	QList<double> mInput;
+	double mOutputReal = 0;
+	double mOutputRequired = 0;
+	double mBase = 0;
+	QList<double> mWeight;
 
 };
 
