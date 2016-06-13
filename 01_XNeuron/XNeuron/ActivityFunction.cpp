d0a53db8e8471950370b2b0ffca3c9d985cbe568
Xma1 <xma1@xma1.de>
2016-06-13 20:53:51 +0200

    [experiment] experiment with function

--- 01_XNeuron/XNeuron/ActivityFunction.cpp
+++ 01_XNeuron/XNeuron/ActivityFunction.cpp
@@ -18,7 +18,7 @@ double ActivityFunction::giveActivityFunction(ActFunction func, double out, doub
     case ActivityFunction::Binary:
         return BinaryFunc(out, bias);
     case ActivityFunction::Line:
-        return LineFunc(out, bias);
+        return LineFunc(out, bias,round);
     case ActivityFunction::Line2:
         break;
     case ActivityFunction::logistic:
@@ -40,7 +40,9 @@ double ActivityFunction::BinaryFunc(double out, double bias)
 
 }
 
-double ActivityFunction::LineFunc(double out, double bias)
+double ActivityFunction::LineFunc(double out, double bias, int round)
 {
+    if(round>0)
+        return ((double)((int)((out+bias)*round)))/round;
     return out+bias;
 }
--- 01_XNeuron/XNeuron/gradienttraining.cpp
+++ 01_XNeuron/XNeuron/gradienttraining.cpp
@@ -8,77 +8,71 @@ GradientTraining::GradientTraining()
 
 bool GradientTraining::train(QList<QList<bool>> &xInput, QList<bool> &xOutputRequired)
 {
-	QList<QList<double>> mInput;
-	QList<double> mOutputRequired;
+    QList<QList<double>> mInput;
+    QList<double> mOutputRequired;
 
-	for (QList<bool>& x : xInput)
-	{
-		QList<double> temp;
-		for (bool& y : x)
-		{
-			temp.append((y) ? (1) : (-1));
-		}
-		mInput.append(temp);
-		mOutputRequired.append((xOutputRequired[xInput.indexOf(x)]) ? (1) : (-1));
-	}
-	return train(mInput, mOutputRequired, ActivityFunction::Binary);
+    for (QList<bool>& x : xInput)
+    {
+        QList<double> temp;
+        for (bool& y : x)
+        {
+            temp.append((y) ? (1) : (-1));
+        }
+        mInput.append(temp);
+        mOutputRequired.append((xOutputRequired[xInput.indexOf(x)]) ? (1) : (-1));
+    }
+    return train(mInput, mOutputRequired, ActivityFunction::Binary);
 }
 
 bool GradientTraining::train(QList<QList<double>> &xInput, QList<double> &xOutputRequired, ActivityFunction::ActFunction xFunc)
 {
-	mFunc = xFunc;
-	qsrand(QTime::currentTime().msec());
-	int check = 5;
-	for (int var = 0; true; var++)
-	{
-		double N = 1;
-		do
-		{
-			N = ((double)((qrand()) % 1000)) / (1000);
-		} while (N == 0);
+    mFunc = xFunc;
+    qsrand(QTime::currentTime().msec());
+    int check = 5;
+    double N = 1;
+    double mDelta=10000;
+    double tDelta=10000;
+    for (int var = 0; true; var++)
+    {
+        do
+        {
+            N = ((double)((qrand()) % 1000)) / (1000);
+        } while (N == 0);
 
-		double error = xInput.size() + check;
-		for (QList<double>& x : xInput)
-		{
-			setInput(x);
-			int tIndexOfInput = xInput.indexOf(x);
+        double error = xInput.size() + check;
 
-			double mDelta = xOutputRequired[tIndexOfInput] - mOutput;
+        for (QList<double>& x : xInput)
+        {
+            tDelta = mDelta;
+            setInput(x);
+            int tIndexOfInput = xInput.indexOf(x);
+            mDelta = xOutputRequired[tIndexOfInput] - mOutput;
+
             if (/*abs(mDelta) > 10 || */std::isnan(mDelta) || std::isinf(mDelta))
-			{
+            {
                 //mBias = 0;
-				for (int i = 0; i < mWeight.length(); i++)
-				{
-                    mWeight[i] = ((double)(qrand()%10000))/1000;
-				}
-			}
-			else  if (mDelta != 0)
-			{
-                //mBias = mBias - N*mDelta;
-
-//				double tMax;
-//				tMax = abs(mInput[0]);
-//                for(double var : mInput)
-//				{
-//					if (tMax < abs(var))
-//						tMax = abs(var);
-//				}
-
-				for (int i = 0; i < mWeight.length(); i++)
-				{
+                for (int i = 0; i < mWeight.length(); i++)
+                {
+                    mWeight[i] = ((double)(qrand()%2000))/1000;
+                }
+            }
+            else  if (mDelta != 0)
+            {
+                for (int i = 0; i < mWeight.length(); i++)
+                {
                     mWeight[i] = mWeight[i] + N*mDelta*mInput[i];
-				}
-			}
-			else
-				error--;
-		}
-		if (error <= check)
-			check--;
-		if (error <= 0)
-			return true;
-	}
+                }
+            }
+            else
+                error--;
+        }
+        if (error <= check)
+            check--;
+        if (error <= 0)
+            return true;
+    }
 
-	return false;
+    return false;
 }
 
 void GradientTraining::setInput(const QList<double> &input)
