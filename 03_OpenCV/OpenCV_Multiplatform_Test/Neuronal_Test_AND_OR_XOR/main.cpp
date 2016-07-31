e6087d76ee5d47781a4fdec8038e841aa108e915
Xma1 <xma1@xma1.de>
2016-07-31 23:31:15 +0200

    [impl] OpenCV ANN Image modification

--- 03_OpenCV/OpenCV_Multiplatform_Test/Neuronal_Test_AND_OR_XOR/main.cpp
+++ 03_OpenCV/OpenCV_Multiplatform_Test/Neuronal_Test_AND_OR_XOR/main.cpp
@@ -10,11 +10,11 @@ using namespace std;
 
 int main(int, char**)
 {
-
-       int inputLayerSize = 2;
+       //0 = Xor; 0.1 = Or; 0.2 = And; 0.3 = Add
+       int inputLayerSize = 4;
        int outputLayerSize = 1;
-       int numSamples = 4;
-       vector<int> layerSizes = { inputLayerSize, 2 ,outputLayerSize };
+       int numSamples = 16;
+       vector<int> layerSizes = { inputLayerSize, 3 ,outputLayerSize };
        Ptr<ml::ANN_MLP> nnPtr = ml::ANN_MLP::create();
        nnPtr->setLayerSizes( layerSizes );
        nnPtr->setActivationFunction( cv::ml::ANN_MLP::SIGMOID_SYM );
@@ -26,19 +26,96 @@ int main(int, char**)
        nnPtr->setTermCriteria(tc);
 
        Mat samples( Size( inputLayerSize, numSamples ), CV_32F );
+      //Xor
        samples.at<float>( Point( 0, 0 ) ) = -1.0f;
        samples.at<float>( Point( 0, 1 ) ) = -1.0f;
        samples.at<float>( Point( 0, 2 ) ) = 1.0f;
        samples.at<float>( Point( 0, 3 ) ) = 1.0f;
+
        samples.at<float>( Point( 1, 0 ) ) = -1.0f;
        samples.at<float>( Point( 1, 1 ) ) = 1.0f;
        samples.at<float>( Point( 1, 2 ) ) = -1.0f;
        samples.at<float>( Point( 1, 3 ) ) = 1.0f;
-       Mat responses( Size( outputLayerSize, numSamples ), CV_32F );
+
+       samples.at<float>( Point( 2, 0 ) ) = 0.0f;
+       samples.at<float>( Point( 2, 1 ) ) = 0.0f;
+       samples.at<float>( Point( 2, 2 ) ) = 0.0f;
+       samples.at<float>( Point( 2, 3 ) ) = 0.0f;
+
+       //Or
+        samples.at<float>( Point( 0, 4 ) ) = -1.0f;
+        samples.at<float>( Point( 0, 5 ) ) = -1.0f;
+        samples.at<float>( Point( 0, 6 ) ) = 1.0f;
+        samples.at<float>( Point( 0, 7 ) ) = 1.0f;
+
+        samples.at<float>( Point( 1, 4 ) ) = -1.0f;
+        samples.at<float>( Point( 1, 5 ) ) = 1.0f;
+        samples.at<float>( Point( 1, 6 ) ) = -1.0f;
+        samples.at<float>( Point( 1, 7 ) ) = 1.0f;
+
+        samples.at<float>( Point( 2, 4 ) ) = 0.1f;
+        samples.at<float>( Point( 2, 5 ) ) = 0.1f;
+        samples.at<float>( Point( 2, 6 ) ) = 0.1f;
+        samples.at<float>( Point( 2, 7 ) ) = 0.1f;
+
+        //And
+        samples.at<float>( Point( 0, 8 ) ) = -1.0f;
+        samples.at<float>( Point( 0, 9 ) ) = -1.0f;
+        samples.at<float>( Point( 0, 10 ) ) = 1.0f;
+        samples.at<float>( Point( 0, 11 ) ) = 1.0f;
+
+        samples.at<float>( Point( 1, 8 ) ) = -1.0f;
+        samples.at<float>( Point( 1, 9 ) ) = 1.0f;
+        samples.at<float>( Point( 1, 10 ) ) = -1.0f;
+        samples.at<float>( Point( 1, 11 ) ) = 1.0f;
+
+        samples.at<float>( Point( 2, 8 ) ) = 0.2f;
+        samples.at<float>( Point( 2, 9 ) ) = 0.2f;
+        samples.at<float>( Point( 2, 10 ) ) = 0.2f;
+        samples.at<float>( Point( 2, 11 ) ) = 0.2f;
+
+        //Add
+        samples.at<float>( Point( 0, 12 ) ) = 0.100f;
+        samples.at<float>( Point( 0, 13 ) ) = 0.045f;
+        samples.at<float>( Point( 0, 14 ) ) = 0.03f;
+        samples.at<float>( Point( 0, 15 ) ) = 0.012f;
+
+        samples.at<float>( Point( 1, 12 ) ) = 0.100f;
+        samples.at<float>( Point( 1, 13 ) ) = 0.015f;
+        samples.at<float>( Point( 1, 14 ) ) = 0.050f;
+        samples.at<float>( Point( 1, 15 ) ) = 0.0f;
+
+        samples.at<float>( Point( 2, 12 ) ) = 0.3f;
+        samples.at<float>( Point( 2, 13 ) ) = 0.3f;
+        samples.at<float>( Point( 2, 14 ) ) = 0.3f;
+        samples.at<float>( Point( 2, 15 ) ) = 0.3f;
+
+
+        Mat responses( Size( outputLayerSize, numSamples ), CV_32F );
+
+        //Xor
        responses.at<float>( Point( 0, 0 ) ) = -1.0f;
        responses.at<float>( Point( 0, 1 ) ) = 1.0f;
        responses.at<float>( Point( 0, 2 ) ) = 1.0f;
        responses.at<float>( Point( 0, 3 ) ) = -1.0f;
+
+       //Or
+       responses.at<float>( Point( 0, 4 ) ) = -1.0f;
+       responses.at<float>( Point( 0, 5 ) ) = 1.0f;
+       responses.at<float>( Point( 0, 6 ) ) = 1.0f;
+       responses.at<float>( Point( 0, 7 ) ) = 1.0f;
+
+       //And
+       responses.at<float>( Point( 0, 8 ) ) = -1.0f;
+       responses.at<float>( Point( 0, 9 ) ) = -1.0f;
+       responses.at<float>( Point( 0, 10 ) ) = -1.0f;
+       responses.at<float>( Point( 0, 11 ) ) = 1.0f;
+
+       //Add
+       responses.at<float>( Point( 0, 12 ) ) = 0.200f;
+       responses.at<float>( Point( 0, 13 ) ) = 0.060f;
+       responses.at<float>( Point( 0, 14 ) ) = 0.080f;
+       responses.at<float>( Point( 0, 15 ) ) = 0.012f;
 
        cout << "samples:\n" << samples << endl;
        cout << "\nresponses:\n" << responses << endl;
@@ -50,6 +127,29 @@ int main(int, char**)
        cout << "\nweights[1]:\n" << nnPtr->getWeights( 1 ) << endl;
        cout << "\nweights[2]:\n" << nnPtr->getWeights( 2 ) << endl;
        cout << "\nweights[3]:\n" << nnPtr->getWeights( 3 ) << endl;
+
+       //Add test values:
+
+       //Add
+       samples.at<float>( Point( 0, 12 ) ) = 0.01f;
+       samples.at<float>( Point( 0, 13 ) ) = 0.001f;
+       samples.at<float>( Point( 0, 14 ) ) = 0.025f;
+       samples.at<float>( Point( 0, 15 ) ) = -0.010f;
+
+       samples.at<float>( Point( 1, 12 ) ) = 0.01f;
+       samples.at<float>( Point( 1, 13 ) ) = 0.02f;
+       samples.at<float>( Point( 1, 14 ) ) = 0.025f;
+       samples.at<float>( Point( 1, 15 ) ) = 0.005f;
+
+       samples.at<float>( Point( 2, 12 ) ) = 0.3f;
+       samples.at<float>( Point( 2, 13 ) ) = 0.3f;
+       samples.at<float>( Point( 2, 14 ) ) = 0.3f;
+       samples.at<float>( Point( 2, 15 ) ) = 0.3f;
+
+       responses.at<float>( Point( 0, 12 ) ) = 0.02f;
+       responses.at<float>( Point( 0, 13 ) ) = 0.021f;
+       responses.at<float>( Point( 0, 14 ) ) = 0.050f;
+       responses.at<float>( Point( 0, 15 ) ) = -0.005f;
 
        Mat output;
        nnPtr->predict( samples, output );
---
+++ 03_OpenCV/OpenCV_Multiplatform_Test/poseevaluator_dataset/main.cpp
@@ -0,0 +1,176 @@
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/ml.hpp>
+#include <iostream>
+#include <vector>
+#include <QList>
+#include <QFileInfo>
+#include <QString>
+#include <QDir>
+#include <QDateTime>
+#include <QDebug>
+
+using namespace cv;
+using namespace std;
+
+int main(int, char**)
+{
+
+    QDateTime start = QDateTime::currentDateTime();
+    qDebug() << "start time: " << start << "\n";
+    QList<Mat> tInput;
+    int file=1;
+    QString Name=QDir().currentPath()+"/Input/"+QString::number(file++)+".jpg";
+    do
+    {
+        if(QFileInfo(Name).exists())
+        {
+            Mat temp=imread(Name.toStdString());
+            pyrDown( temp, temp );
+            tInput.append(temp);
+            Name=QDir().currentPath()+"/Input/"+QString::number(file++)+".jpg";
+        }
+    }while(QFileInfo(Name).exists());
+
+
+
+    QList<Mat> tResponses;
+    file=1;
+    Name=QDir().currentPath()+"/Input/"+QString::number(file++)+".jpg";
+    do
+    {
+        Mat temp=imread(Name.toStdString());
+        pyrDown( temp, temp );
+        tResponses.append(temp);
+        Name=QDir().currentPath()+"/Responses/"+QString::number(file++)+".jpg";
+    }while(QFileInfo(Name).exists());
+
+
+    int max=tInput[0].rows*tInput[0].cols*3;
+    QList<QList<float>> tBWList;
+    QList<QList<float>> tColorList;
+    int w,h;
+    h=tInput[0].rows;
+    w=tInput[0].cols*3;
+
+    for(int m=0;m<tInput.length();m++)
+    {
+        Mat tBW=tInput[m];
+
+        if(max>tBW.rows*tBW.cols*3)
+        {   h=tBW.rows;
+            w=tBW.cols;
+            max=tBW.rows*tBW.cols*3;
+        }
+        auto tPtr=tBW.ptr<uchar>(0);
+        QList<float> tBWRow;
+        for (int i = 0; i < tBW.rows; ++i)
+        {
+            for (int j = 0; j < tBW.cols; ++j)
+            {
+                tBWRow.append((float)(*tPtr++)/255);
+                tBWRow.append((float)(*tPtr++)/255);
+                tBWRow.append((float)(*tPtr++)/255);
+            }
+        }
+        tBWList.append(tBWRow);
+    }
+
+    for(int m=0; m<tResponses.length();m++)
+    {
+        Mat tColor = tResponses[m];
+        if(max>tColor.rows*tColor.cols*3)
+        {
+            h=tColor.rows;
+            w=tColor.cols;
+            max=tColor.rows*tColor.cols*3;
+        }
+        auto tPtr=tColor.ptr<uchar>(0);
+        QList<float> tColorRow;
+        for (int i = 0; i < h; ++i)
+        {
+            for (int j = 0; j < w; ++j)
+            {
+                tColorRow.append((float)(*tPtr++)/255);
+                tColorRow.append((float)(*tPtr++)/255);
+                tColorRow.append((float)(*tPtr++)/255);
+            }
+        }
+        tColorList.append(tColorRow);
+    }
+
+    int inputLayerSize = max;
+    int outputLayerSize = max;
+    int numSamples = tColorList.length();
+
+    Mat samples( Size( inputLayerSize, numSamples ), CV_32F );
+    Mat responses( Size( outputLayerSize, numSamples ), CV_32F );
+
+    int index=0;
+    while(index < max)
+    {
+        for(int s = 0; s < tColorList.length(); s++)
+        {
+            samples.at<float>( Point( index, s ) ) = tBWList[s][index];
+            responses.at<float>( Point( index, s ) ) = tColorList[s][index];
+        }
+        index++;
+    }
+
+    vector<int> layerSizes = { inputLayerSize, 50, inputLayerSize };
+    Ptr<ml::ANN_MLP> nnPtr;
+    if(QFileInfo("Color.yml").exists())
+    {
+        cout << "Loading\n" << endl;
+        nnPtr=Algorithm::load<ml::ANN_MLP>("Color.yml");
+    }
+    else
+    {
+        nnPtr = ml::ANN_MLP::create();
+
+        nnPtr->setLayerSizes( layerSizes );
+        nnPtr->setActivationFunction( cv::ml::ANN_MLP::SIGMOID_SYM );
+        nnPtr->setTrainMethod(ml::ANN_MLP::BACKPROP,0.01,0.01);
+
+        TermCriteria tc;
+        tc.epsilon=0.01;
+        tc.maxCount=10000;
+        tc.type = TermCriteria::MAX_ITER + TermCriteria::EPS;
+
+        cout << "begin training:\n" << endl;
+        if ( !nnPtr->train( samples, ml::ROW_SAMPLE, responses ) )
+            return 1;
+        cout << "end training:\n" << endl;
+        nnPtr->save("Color.yml");
+        QDateTime end = QDateTime::currentDateTime();
+        qDebug() << "end training: " << end << "\n";
+        qDebug() << "diff time training: " << start.msecsTo(end) << "\n";
+    }
+
+    cout << "Predicting\n" << endl;
+    Mat output;
+    nnPtr->predict( samples, output );
+
+    cout << "Saving\n" << endl;
+    for( int r=0; r < output.rows;r++)
+    {
+        index=0;
+        Mat image2( Size( w, h ), CV_32FC3);
+        auto Out1=output.ptr<float>(r);
+        auto im1=image2.ptr<float>(0);
+        for( int c=0; c < output.cols;c++)
+        {
+            *im1=Out1[c];
+            im1++;
+            index++;
+        }
+        Name=QDir().currentPath()+"/Output/"+QString::number(r)+".jpg";
+        imwrite( Name.toStdString(), image2 );
+    }
+    cout << "Finished\n" << endl;
+    QDateTime end = QDateTime::currentDateTime();
+    qDebug() << "end time: " << end << "\n";
+    qDebug() << "diff time: " << start.msecsTo(end) << "\n";
+    return 0;
+}
